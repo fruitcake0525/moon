234ac8c048896b34fc378e198b604f524a1578a1
tangjiahong <fruitcake0525@gmail.com>
2016-08-07 22:04:21 +0800

    avoid destroy email

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   root 'landingpage#index'
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+#  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, skip: :registrations
+  devise_scope :user do
+  resource :registration,
+    only: [:new, :create, :edit, :update],
+    path: 'users',
+    path_names: { new: 'sign_up' },
+    controller: 'devise/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    as: :user_registration do
+      get :cancel
+    end
+  end
 
   namespace :admin do
     resources :projects
