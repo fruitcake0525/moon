e31025768036c51c7222bc08569d3637860ef220
tangjiahong <fruitcake0525@gmail.com>
2016-08-08 00:16:55 +0800

    fix facebook API

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,8 +243,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['facebook_id'], ENV['faebook_secret']
-  config.omniauth :google_oauth2, , ENV['google_id'], ENV['google_secret']
-  config.omniauth :github, , ENV['github_id'], ENV['github_secret'], scope: 'user,public_repo'
+  config.omniauth :google_oauth2, ENV['google_id'], ENV['google_secret']
+  config.omniauth :github, ENV['github_id'], ENV['github_secret'], scope: 'user,public_repo'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
