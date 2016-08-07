28bec4ade1c4f0d5a154cfea5511b94ff587cbcc
tangjiahong <fruitcake0525@gmail.com>
2016-08-07 22:12:42 +0800

    fix omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
 #  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_for :users, skip: :registrations
   devise_scope :user do
-  resource :registration,
-    only: [:new, :create, :edit, :update],
-    path: 'users',
-    path_names: { new: 'sign_up' },
-    controller: 'devise/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    as: :user_registration do
-      get :cancel
-    end
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      :omniauth_callbacks => "users/omniauth_callbacks",
+      as: :user_registration do
+        get :cancel
+      end
   end
 
   namespace :admin do
